15554cff804f5d46999fb8771f2480ecbc8087ad
rkunche <kunche.raju@gmail.com>
2025-08-07 11:23:48 +0530

    Update Action.js
    bug fix test

--- WebHttp_advantage_git/Action.js
+++ WebHttp_advantage_git/Action.js
@@ -7,7 +7,7 @@
 
 function Action()
 {
-	// Sync project in LRC // making edit on 7/10/7/25
+	// Sync project in LRC // making edit on 7/10/7/25- test
 	web.url(
 		{
 			name : 'advantageonlineshopping.com', 
