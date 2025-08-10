bcf322006e869fddaaef2c44ab265fe306091466
rkunche <kunche.raju@gmail.com>
2025-08-10 08:24:00 +0530

    Update Action.js

--- WebHttp_advantage_git/Action.js
+++ WebHttp_advantage_git/Action.js
@@ -7,7 +7,7 @@
 
 function Action()
 {
-	// Sync project in LRC // making edit on 7/10/7/25- test123
+	// Sync project in LRC // making edit on 7/10/7/25- test12345
 	web.url(
 		{
 			name : 'advantageonlineshopping.com', 
