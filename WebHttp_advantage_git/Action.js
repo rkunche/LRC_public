f3ca1a65fe62531af64560f88f18cc54ccdca05b
rkunche <kunche.raju@gmail.com>
2025-08-10 09:07:24 +0530

    Update Action.js
    hello22 added

--- WebHttp_advantage_git/Action.js
+++ WebHttp_advantage_git/Action.js
@@ -7,7 +7,7 @@
 
 function Action()
 {
-	// Sync project in LRC // making edit on 7/10/7/25- test-hello11
+	// Sync project in LRC // making edit on 7/10/7/25- test-hello22
 	web.url(
 		{
 			name : 'advantageonlineshopping.com', 
