773019df0779e6b6e6d11564c9d028a8fa9dd888
rkunche <kunche.raju@gmail.com>
2026-01-18 10:18:13 +0530

    Update Action.js
    ssh auth test

--- WebHttp_advantage_git/Action.js
+++ WebHttp_advantage_git/Action.js
@@ -7,7 +7,7 @@
 
 function Action()
 {
-	// Sync project in LRC // making edit on 7/10/7/25- multi-project-httpauth test
+	// Sync project in LRC // making edit on 7/10/7/25- multi-project-httpauth test-ssh
 	web.url(
 		{
 			name : 'advantageonlineshopping.com', 
